a3f7b58ed2277b07cb6f0c45ac866f1f83653f0d
“emre_senturk” <emre.senturk@commencis.com>
2022-08-15 13:59:09 +0300

    provider fix

--- cloudwatch-module/main.tf
+++ cloudwatch-module/main.tf
@@ -1,14 +1,15 @@
+
+
 terraform {
   required_version = ">= 0.13.1"
 
-  provider {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.63"
     }
   }
 }
-
 module "cloudwatch" {
   source = "../"
   
